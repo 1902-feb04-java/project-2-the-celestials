7c808c05ce41acf4e7b424f260d89fd9754ece3b
Chad Wilson <cwilson66@mail.csuchico.edu>
2019-03-14 11:29:45 -0500

    Creates AbstractLoreItem, LoreTableclasses and updates World and Tag class

--- test/src/main/java/com/example/demo/test/Tag.java
+++ test/src/main/java/com/example/demo/test/Tag.java
@@ -1,23 +1,16 @@
 package com.example.demo.test;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 
 @Entity
-public class Tags {
+public class Tag extends LoreTable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private int id;
-	private String name;
-
-	/*@ManyToMany(mappedBy = "tags")
-	private List<Worlds> worlds = new ArrayList<>();*/
 
 	public int getId() {
 		return id;
@@ -35,20 +28,12 @@ public class Tags {
 		this.name = name;
 	}
 
-	/*public List<Worlds> getWorlds() {
-		return worlds;
-	}
-
-	public void setWorlds(List<Worlds> worlds) {
-		this.worlds = worlds;
-	}
-
 	@Override
 	public String toString() {
-		return "Tags [id=" + id + ", name=" + name + ", worlds=" + worlds + "]";
-	}*/
+		return "Tags [id=" + id + ", name=" + name + "]";
+	}
 
-	public Tags() {
+	public Tag() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
