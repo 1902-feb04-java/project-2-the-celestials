081664c6ab789d4c8d5a650ed652a06ecd86a99b
jburtson <jburtson@gmail.com>
2019-03-19 14:40:15 -0500

    added a controller

--- test/src/main/java/com/example/demo/test/LoreTable.java
+++ test/src/main/java/com/example/demo/test/LoreTable.java
@@ -15,4 +15,21 @@ public abstract class LoreTable {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="lore_seq")
 	protected int id;	
 	protected String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
